f4efb377649c23b0004c24da2c83a807f4e142a9
DaoVanThanh <thanhap03@gmail.com>
2023-06-08 12:41:46 +0700

    Build get, post REST API

---
+++ src/main/java/com/example/springboot/controller/EmployeeController.java
@@ -0,0 +1,31 @@
+package com.example.springboot.controller;
+
+import com.example.springboot.model.Employee;
+import com.example.springboot.service.EmployeeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+    private EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    //Build create employee REST API
+    @PostMapping
+    public ResponseEntity<Employee> saveEmployee(@RequestBody Employee employee) {
+        return new ResponseEntity<Employee>(employeeService.saveEmployee(employee), HttpStatus.CREATED);
+    }
+
+    //Build get all employees REST API
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        return employeeService.getAllEmployees();
+    }
+}
